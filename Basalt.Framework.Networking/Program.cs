d49a12c893f2f65946320bbdd553db5e2c99cce9
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-09 15:39:01 -0400

    core: setup project properties

--- Basalt.Framework.Networking/Program.cs
+++ Basalt.Framework.Networking/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Basalt.Framework.Networking;
+﻿using System;
+
+namespace Basalt.Framework.Networking;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.ReadKey();
     }
 }
