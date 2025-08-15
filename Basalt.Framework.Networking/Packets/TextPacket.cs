6dbeba2fa5767ac8e58244cbbf552ad466c9a873
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-15 19:57:53 -0400

    core: remove extra stuff

--- Basalt.Framework.Networking/Packets/TextPacket.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace Basalt.Framework.Networking.Packets;
-
-public class TextPacket : BasePacket
-{
-    public string Text { get; set; } = string.Empty;
-}
--- Basalt.Framework.Networking/QueuedTcpClient.cs
+++ Basalt.Framework.Networking/QueuedTcpClient.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 
 namespace Basalt.Framework.Networking;
 
-public class QueuedTcpClient
+internal class QueuedTcpClient
 {
     private readonly TcpClient _client;
     private readonly List<byte> _queue;
